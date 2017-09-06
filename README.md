d84164f414276b2f142d13d26d061e11f914999b
Dave Hylands <dhylands@gmail.com>
2017-09-06 09:07:49 -0700

    Initial support for BrainPad

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# BRAINPAD
-MicroPython board files for the GHI Electronics BrainPad 2 board.
+This is a MicroPython board definition file for the BrainPad2 from GHI Electronics.
+
+The product page for the BrainPad2 can be found here: http://docs.ghielectronics.com/hardware/products/brainpad.html
+
+Build the firmware using:
+```
+cd micropython/ports/stm32/boards
+git clone https://github.com/dhylands/BRAINPAD.git
+cd ..
+make BOARD=BRAINPAD
+```
+
+To get the BrainPad into DFU mode, press and hold the BOOT0 pushbutton
+while plugging in the USB cable.
+
+Once in DFU mode, you should be able to use the ```dfu-util --list``` command and see
+something like this:
+```
+Found DFU: [0483:df11] ver=2200, devnum=9, cfg=1, intf=0, alt=3, name="@Device Feature/0xFFFF0000/01*004 e", serial="3473375A3334"
+Found DFU: [0483:df11] ver=2200, devnum=9, cfg=1, intf=0, alt=2, name="@OTP Memory /0x1FFF7800/01*512 e,01*016 e", serial="3473375A3334"
+Found DFU: [0483:df11] ver=2200, devnum=9, cfg=1, intf=0, alt=1, name="@Option Bytes  /0x1FFFC000/01*016 e", serial="3473375A3334"
+Found DFU: [0483:df11] ver=2200, devnum=9, cfg=1, intf=0, alt=0, name="@Internal Flash  /0x08000000/04*016Kg,01*064Kg,03*128Kg", serial="3473375A3334"
+```
+
+Setup udev rules as described [here](https://github.com/micropython/micropython/wiki/Board-STM32F407-Discovery)
+
+Now you can flash MicroPython using:
+```
+make BOARD=BRAINPAD deploy
+```
+
+The board should reboot, and you should then be able to connect to the MicroPython REPL
+on the usb serial port (typically /dev/ttyACM0):
+```
+MicroPython v1.9.2-60-gbeeb748 on 2017-09-06; BRAINPAD with STM32F401RE
+Type "help()" for more information.
+>>> 
+```
+
+Now that MicroPython is installed, as long as you can get to the REPL, then you
+can get back into DFU mode by using the command:
+```
+>>> pyb.bootloader()
+```
+or by pressing and holding BOOT0 while plugging in the USB cable.
+
+Currently, the L pushbutton is connected to the User Switch.
+
+## Peripheral Testing status:
+
+Tested:
+- USB Repl/Mass Storage
+- LED (including PWM intensity)
+
+To be tested:
+- Push Buttons
+- Piezo Buzzer
+- Servo
+- LCD
+- Accelerometer
+- Temperature Sensor
+- Light Sensor
---
+++ mpconfigboard.h
@@ -0,0 +1,76 @@
+#define MICROPY_HW_BOARD_NAME       "BRAINPAD"
+#define MICROPY_HW_MCU_NAME         "STM32F401RE"
+#define MICROPY_PY_SYS_PLATFORM     "pyboard"
+
+#define MICROPY_HW_HAS_SWITCH       (1)
+#define MICROPY_HW_HAS_FLASH        (1)
+#define MICROPY_HW_HAS_SDCARD       (0)
+#define MICROPY_HW_HAS_MMA7660      (0)
+#define MICROPY_HW_HAS_LIS3DSH      (0)
+#define MICROPY_HW_HAS_LCD          (0)
+#define MICROPY_HW_ENABLE_RNG       (0)
+#define MICROPY_HW_ENABLE_RTC       (0)
+#define MICROPY_HW_ENABLE_TIMER     (1)
+#define MICROPY_HW_ENABLE_SERVO     (1)
+#define MICROPY_HW_ENABLE_DAC       (0)
+#define MICROPY_HW_ENABLE_CAN       (0)
+
+// HSE is 8 MHz - F401 does 84 MHz max
+#define MICROPY_HW_CLK_PLLM (8)
+#define MICROPY_HW_CLK_PLLN (336)
+#define MICROPY_HW_CLK_PLLP (RCC_PLLP_DIV4)
+#define MICROPY_HW_CLK_PLLQ (7)
+
+// The BRAINPAD doesn't have an RTC crystal.
+#define MICROPY_HW_RTC_USE_LSE      (0)
+#define MICROPY_HW_RTC_USE_US       (0)
+#define MICROPY_HW_RTC_USE_CALOUT   (0)
+
+// UART config
+#define MICROPY_HW_UART1_PORT (GPIOA)
+#define MICROPY_HW_UART1_PINS (GPIO_PIN_9 | GPIO_PIN_10)
+
+#define MICROPY_HW_UART2_PORT (GPIOA)
+#define MICROPY_HW_UART2_PINS (GPIO_PIN_2 | GPIO_PIN_3)
+#define MICROPY_HW_UART2_RTS  (GPIO_PIN_1) // doesn't seem to be accessible on BrainPad
+#define MICROPY_HW_UART2_CTS  (GPIO_PIN_0)
+
+// I2C busses
+#define MICROPY_HW_I2C1_SCL (pin_B6)
+#define MICROPY_HW_I2C1_SDA (pin_B7)
+
+#define MICROPY_HW_I2C3_SCL (pin_A8)
+#define MICROPY_HW_I2C3_SDA (pin_B4)
+
+// SPI busses
+
+// We don't use NSS, but need to define it.
+#define MICROPY_HW_SPI1_NSS  (pin_C3)
+
+#define MICROPY_HW_SPI1_SCK  (pin_B3)
+#define MICROPY_HW_SPI1_MISO (pin_B4)
+#define MICROPY_HW_SPI1_MOSI (pin_B5)
+
+// The BrainPad has an RGB LED, so we map LED 1 to Red, 2 to Greem and 3 to Blue
+#define MICROPY_HW_LED1             (pin_C9)
+#define MICROPY_HW_LED1_PWM         { TIM3, 3, TIM_CHANNEL_4, GPIO_AF2_TIM3 }
+#define MICROPY_HW_LED_OTYPE        (GPIO_MODE_OUTPUT_PP)
+#define MICROPY_HW_LED_ON(pin)      (mp_hal_pin_high(pin))
+#define MICROPY_HW_LED_OFF(pin)     (mp_hal_pin_low(pin))
+
+#define MICROPY_HW_LED2             (pin_C8)
+#define MICROPY_HW_LED2_PWM         { TIM3, 3, TIM_CHANNEL_3, GPIO_AF2_TIM3 }
+
+#define MICROPY_HW_LED3             (pin_C6)
+#define MICROPY_HW_LED3_PWM         { TIM3, 3, TIM_CHANNEL_1, GPIO_AF2_TIM3 }
+
+// There are 4 switches, but the current code only supports one
+// So I picked LDR0
+#define MICROPY_HW_USRSW_PIN        (pin_A15)
+#define MICROPY_HW_USRSW_PULL       (GPIO_PULLUP)
+#define MICROPY_HW_USRSW_EXTI_MODE  (GPIO_MODE_IT_FALLING)
+#define MICROPY_HW_USRSW_PRESSED    (0)
+
+// USB config
+//#define MICROPY_HW_USB_VBUS_DETECT_PIN (pin_A9)
+//#define MICROPY_HW_USB_OTG_ID_PIN      (pin_A10)
